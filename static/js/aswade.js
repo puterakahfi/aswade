04eb245bd173092fcbd8cd2104d7a132dcf4291c
puterakahfi <puterakahfi@gmail.com>
2015-01-24 17:41:14 +0700

    modified some files

--- static/js/aswade.js
+++ static/js/aswade.js
@@ -1,1 +1,8 @@
-var app= angular.module('aswadeApp',[]);
+var app = angular.module('aswadeApp', ['mm.foundation']);
+app.controller('GalleryCtrl', function ($scope) {
+
+    this.detailGallery = function () {
+        alert('test');
+    };
+    return $scope.AppCtrl = this;
+});
